e69075dab43455434a28e058fd218c67b5942502
unknown <A051363@PCS8556.GVA.ICRC.Priv>
2015-04-24 15:49:07 +0200

    erl edit

--- src/nnerl.erl
+++ src/nnerl.erl
@@ -1,9 +1,25 @@
+-module(nnerl).
 -behaviour(application).
 -export([start/2, stop/1, start_pool/3,
         run/2, sync_queue/2, async_queue/2, stop_pool/1]).
  
 start(normal, _Args) ->
-    ppool_supersup:start_link().
+    nnerl_supersup:start_link().
  
 stop(_State) ->
+    ok.
+
+run(A, B) ->
+    ok.
+
+sync_queue(A, B) ->
+    ok.
+
+async_queue(A, B) ->
+    ok.
+
+start_pool(A, B, C) ->
+    ok.
+
+stop_pool(A) ->
     ok.
--- src/nnerl_serv.erl
+++ src/nnerl_serv.erl
@@ -1,8 +1,7 @@
 -module(nnerl_serv).
 -behaviour(gen_server).
 -export([start/4, start_link/4, run/2, sync_queue/2, async_queue/2, stop/1]).
--export([init/1, handle_call/3, handle_cast/2, handle_info/2,
-        code_change/3, terminate/2]).%% The friendly supervisor is started dynamically!
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2, code_change/3, terminate/2]).%% The friendly supervisor is started dynamically!
 -define(SPEC(MFA),
     {worker_sup,
         {ppool_worker_sup, start_link, [MFA]},
@@ -10,7 +9,16 @@
         10000,
         supervisor,
         [ppool_worker_sup]}).
- 
+
+alloc(Chs) ->
+    gen_server:call(ch3, alloc).
+
+free(Ch, Chs) ->
+    gen_server:cast(ch3, {free, Ch}).
+
+channels() ->
+    ok.
+
 start(Name, Limit, Sup, MFA) when is_atom(Name), is_integer(Limit) ->
     gen_server:start({local, Name}, ?MODULE, {Limit, MFA, Sup}, []).
  
@@ -28,3 +36,25 @@ async_queue(Name, Args) ->
  
 stop(Name) ->
     gen_server:call(Name, stop).
+
+init(_Args) ->
+    {ok, channels()}.
+
+handle_cast(stop, State) ->
+    {stop, normal, State};
+handle_cast({free, Ch}, Chs) ->
+    Chs2 = free(Ch, Chs),
+    {noreply, Chs2}.
+
+handle_call(alloc, _From, Chs) ->
+    {Ch, Chs2} = alloc(Chs),
+    {reply, Ch, Chs2}.
+
+handle_info({'EXIT', Pid, Reason}, State) ->
+    {noreply, State}.
+
+code_change(OldVsn, State, Extra) ->
+    {ok, State}.
+
+terminate(shutdown, State) ->
+    ok.
